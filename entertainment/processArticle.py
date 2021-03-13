2d6b1717b61b0fac26e34e26a937b7b0ac38b8cb
mashuk999 <mashuk1998@gmail.com>
2021-03-13 16:41:02 +0530

    automatic push

--- entertainment/processArticle.py
+++ entertainment/processArticle.py
@@ -71,9 +71,9 @@ def getArticleWebpage(url):
 def scrapArticle(web_page):
     soup = BeautifulSoup(web_page, 'html.parser')
     content = soup.find("div", {"class": "article-desc ul_styling"})
-    contentn = content.find("div", {"class": "text-align: justify;"})
-    if contentn is not None:
-        content = contentn
+    # contentn = content.find("div", {"class": "text-align: justify;"})
+    # if contentn is not None:
+    #     content = contentn
 
     for divs in content.findAll("div"):
         divs.extract()
