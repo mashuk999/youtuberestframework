801cc97a4c39858442cba7e04dbf4f5235154084
anugrah2002 <anugrahnigam16@gmail.com>
2022-06-04 17:32:23 +0530

    pushing new branch

--- entertainment_news/cleanupfunction.py
+++ entertainment_news/cleanupfunction.py
@@ -3,12 +3,19 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from entertainment.serializer import *
 from . models import *
+import datetime
 
 
 @renderer_classes((TemplateHTMLRenderer, JSONRenderer))
 @api_view(('GET',))
 def cleanupFunction(request):
     if request.method == 'GET':
-        topics = entertainmentSaveVideonews_for_aajtk.objects.all()
+        NextDay_Date = datetime.datetime.today() - datetime.timedelta(days=1)
+        print(NextDay_Date)
+        formatted = NextDay_Date.strftime("%y-%m-%d")
+        print(formatted)
+        topics = entertainmentSaveVideonews_for_aajtk.objects.filter(videoPublicId=formatted)
+        print(topics)
         serializer = Get_Savevideoserializer_of_aajtk(topics, many=True)
+
         return Response(serializer.data)
